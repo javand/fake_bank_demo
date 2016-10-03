108a9bfcd6ffc8f86a3a449166481b545c648209
javan@taximagic.com <javan@taximagic.com>
2016-10-03 03:36:28 -0400

    tests for protected resources

---
+++ src/test/java/com/fakebank/ProtectedResourceTests.java
@@ -0,0 +1,18 @@
+package com.fakebank;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+/**
+ * Created by Javan on 10/3/16.
+ */
+public class ProtectedResourceTests extends AbstractIntegrationTests {
+
+
+
+}
